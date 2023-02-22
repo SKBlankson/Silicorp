bc1d0bdd966e9902ab6622ccd481501bc537ae96
HenryWood123 <114782296+HenryWood123@users.noreply.github.com>
2023-02-22 22:24:03 +0000

    Add files via upload
    - Changed code to make it accept encrypted passwords

--- login_process.php
+++ login_process.php
@@ -29,7 +29,7 @@
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
     $count = mysqli_num_rows($result);	
 	
-    if($count == 1 && $user_pass == $row['User_password']){//&& password_verify($user_pass, $row['User_password'])
+    if($count == 1 && password_verify($user_pass, $row['User_password']){
 
     	$user_id = $row['User_id']; 
     	$fname = $row['Fname'];
